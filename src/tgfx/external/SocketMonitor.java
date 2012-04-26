7e9ecb035f107398c6e87a2ac1ffc4608b787500
Matt Stock <stock@csgeeks.org>
2012-04-26 07:47:13 -0400

    Changes to exit and close copy thread when network connection closes/errors.

--- src/tgfx/external/SocketMonitor.java
+++ src/tgfx/external/SocketMonitor.java
@@ -70,7 +70,7 @@ public class SocketMonitor {
  */
 
 class ConnectionHandler implements Runnable, Observer {
-
+    private boolean disconnect = false;
     public Socket socket;
 
     @Override
@@ -83,9 +83,10 @@ class ConnectionHandler implements Runnable, Observer {
 //             System.out.println("UPDATE: "+MSG[1]);
             try {
                 this.write(MSG[1] + "\n");
+            } catch (IOException ex) {
+                disconnect = true;
             } catch (Exception ex) {
-                //
-                System.out.println(ex.getMessage());
+                System.out.println("update(): " + ex.getMessage());
             }
 
         }
@@ -122,17 +123,15 @@ class ConnectionHandler implements Runnable, Observer {
             TinygDriver tg = TinygDriver.getInstance();
             String line = "";
             SerialDriver ser = SerialDriver.getInstance();
-            while (ser.isConnected()) {
+            while (ser.isConnected() && !disconnect) {
                 try {
                     line = stdIn.readLine() + "\n";
 //                    this.write("Writing: " + line);
                     tg.write(line);
                     Thread.sleep(100);
                 } catch (Exception ex) {
+                    System.out.println("run(): " + ex.getMessage());
                 }
-
-
-
             }
             System.out.println("[+]Closing Remote Listener Socket");
             socket.close();
