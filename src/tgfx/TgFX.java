e82401b043cf569ffa8d84b9d9a7b414351f84ba
Pat Farrell <pfarrell@pfarrell.com>
2013-12-05 22:30:02 -0500

    trivial cleanup of imports Just to make sure I am cleanly setup in git.

--- src/tgfx/TgFX.java
+++ src/tgfx/TgFX.java
@@ -1,6 +1,7 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (c) 2013 Synthetos LLC
+ * Rileyporter@gmail.com
+ * www.synthetos.com
  */
 package tgfx;
 
