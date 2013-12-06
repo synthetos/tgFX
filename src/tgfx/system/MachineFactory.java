ec84a6c343813aa38baac50b9124cf64f8901384
Pat Farrell <pfarrell@pfarrell.com>
2013-12-06 16:38:10 -0500

    MachineFactory is a factory class that makes a machine. In production, there is only one Machine per application, but for unit testing, we may want different rules. or even a MockMachine

---
+++ src/tgfx/system/MachineFactory.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2013 Synthetos LLC. All Rights reserved.
+ */
+
+package tgfx.system;
+
+import java.util.concurrent.locks.ReadWriteLock;
+/**
+ * The <code> MachineFactory</code> class creates Machines for us. It completely
+ * hides the fact that the Machine is really one instance.
+ * @author pfarrell
+ * Created on Dec 6, 2013 4:23:11 PM
+ */
+public class MachineFactory {
+    private static Machine theMachine;
+    private static ReadWriteLock lock;
+/**
+ * gets a machine.
+ * @return a nice machine
+ */    
+    public static Machine getMachine() {
+        Machine rval = null;
+        lock.writeLock().lock();
+        try {
+            if (theMachine == null) {
+                theMachine = new AbstractMachine();
+            }
+            rval = theMachine;
+        } finally {
+            lock.writeLock().unlock();
+        }
+        return rval;
+    }
+}
