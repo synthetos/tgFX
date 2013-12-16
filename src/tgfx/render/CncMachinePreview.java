71afe298612e1341a31e162a2349b25f0789b6ea
Pat Farrell <pfarrell@pfarrell.com>
2013-12-16 01:16:17 -0500

    Revert "update copyright comment"
    This reverts commit bdbad86ed7b362a2fed5462e45cb50b7ec026870.

--- src/tgfx/render/CncMachinePreview.java
+++ src/tgfx/render/CncMachinePreview.java
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2013 Synthetos LLC. All Rights reserved.
- * see license.txt for terms.
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
  */
 package tgfx.render;
 
@@ -30,7 +30,7 @@ import tgfx.tinyg.TinygDriver;
 import tgfx.ui.gcode.GcodeTabController;
 
 /**
- * The <code>CncMachinePreview</code> is all about drawing the 2d preview.  Meaning.. the table size the lines everything.
+ *
  * @author rileyporter
  */
 public class CncMachinePreview extends Pane {
